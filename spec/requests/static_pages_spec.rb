60a5afd5a5e044e46f3b420a35f9f43cdc1c2381
Christian Briones <cwbriones@gmail.com>
2014-01-06 15:10:48 -0800

    Add title tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,6 +4,11 @@ describe "Static pages" do
 
   describe "Home page" do
 
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial | Home")
+    end
+
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
@@ -12,6 +17,11 @@ describe "Static pages" do
 
   describe "Help page" do
 
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial | Help")
+    end
+
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
@@ -19,6 +29,11 @@ describe "Static pages" do
   end
 
   describe "About page" do
+
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial | About Us")
+    end
 
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
