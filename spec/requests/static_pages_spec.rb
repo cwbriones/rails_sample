d49b061951e04f084cfa752e11e2113504aeb556
Christian Briones <cwbriones@gmail.com>
2014-01-06 20:38:27 -0800

    Remove custom title from static_pages/home

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,9 +6,14 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the right title" do
+    it "should have the right base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
+      expect(page).to have_title(base_title)
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title("| Home")
     end
 
     it "should have the content 'Sample App'" do
