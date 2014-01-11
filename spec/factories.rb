b4166a2e4f160df0bc8435fe101ec0f5085b910a
Christian Briones <cwbriones@gmail.com>
2014-01-11 15:24:06 -0800

    Add user page tests with FactoryGirl gem

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "Christian Briones"
+    email "cbriones@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,11 +1,20 @@
 require 'spec_helper'
 
 describe "UserPages" do
+
   subject { page }
 
   describe "signup page" do
     before { visit signup_path }
     it { should have_content('Sign up') }
     it { should have_title('Sign Up') }
+  end
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
   end
 end
